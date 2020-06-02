93ef7058a12bedbbeb50c539f29dc6e5c875b575
Francis Den Esteban <francis.den.esteban@outlook.com>
2020-06-02 16:15:54 +0800

    Updated EmployeeRepository.java to add comment.

--- src/main/java/com/francis/spring/boot/spring/data/rest/repository/EmployeeRepository.java
+++ src/main/java/com/francis/spring/boot/spring/data/rest/repository/EmployeeRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.francis.spring.boot.spring.data.rest.entity.Employee;
 
+
+/*
+ * This interface will extend the JpaRepository that simplifies data access to the database
+ */
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
